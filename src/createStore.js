b4beed440c9bbbe005ff0c5fd9c6fa63c6821667
Tom Rogers <tom@roguevendor.com>
2021-02-10 01:18:37 -0600

    feat: replaced dispatch with mutate

--- src/createStore.js
+++ src/createStore.js
@@ -1,44 +1,19 @@
-import isPlainObject from './utils/isPlainObject'
-
 /**
- * These are private action types reserved by Redux.
- * For any unknown actions, you must return the current state.
- * If the current state is undefined, you must return the initial state.
- * Do not reference these action types directly in your code.
- */
-export var ActionTypes = {
-  INIT: '@@redux/INIT'
-}
-
-/**
- * Creates a Redux store that holds the state tree.
- * The only way to change the data in the store is to call `dispatch()` on it.
+ * Creates a Mutatex store that holds the state tree.
+ * The only way to change the data in the store is to call `mutate()` on it.
  *
- * There should only be a single store in your app. To specify how different
- * parts of the state tree respond to actions, you may combine several reducers
- * into a single reducer function by using `combineReducers`.
- *
- * @param {Function} reducer A function that returns the next state tree, given
- * the current state tree and the action to handle.
+ * There should only be a single store in your app.
  *
  * @param {any} [initialState] The initial state. You may optionally specify it
  * to hydrate the state from the server in universal apps, or to restore a
  * previously serialized user session.
- * If you use `combineReducers` to produce the root reducer function, this must be
- * an object with the same shape as `combineReducers` keys.
  *
- * @returns {Store} A Redux store that lets you read the state, dispatch actions
+ * @returns {Store} A Mutatex store that lets you read the state, mutate the store,
  * and subscribe to changes.
  */
-export default function createStore(reducer, initialState) {
-  if (typeof reducer !== 'function') {
-    throw new Error('Expected the reducer to be a function.')
-  }
-
-  var currentReducer = reducer
+export default function createStore(initialState) {
   var currentState = initialState
   var listeners = []
-  var isDispatching = false
 
   /**
    * Reads the state tree managed by the store.
@@ -73,84 +48,45 @@ export default function createStore(reducer, initialState) {
   }
 
   /**
-   * Dispatches an action. It is the only way to trigger a state change.
+   * Performs one or more mutations on the store. It is the only way to trigger
+   * state changes.
    *
-   * The `reducer` function, used to create the store, will be called with the
-   * current state tree and the given `action`. Its return value will
-   * be considered the **next** state of the tree, and the change listeners
-   * will be notified.
+   * Each argument must be a reducer function, used to compute the whole new
+   * state. Reducers are run in sequence, one after the other, synchronously.
+   * The first reducer receives as its argument the whole current state, and
+   * its return value becomes the next whole state. If a second reducer
+   * function was provided, it receives as its argument the first reducer's
+   * return value, and whatever its return value becomes the next whole state.
+   * This repeats for every reducer provided to mutate. Subscribers are
+   * notified only after the entire set of reducers has been executed,
+   * yielding the final state for the current action.
    *
-   * The base implementation only supports plain object actions. If you want to
-   * dispatch a Promise, an Observable, a thunk, or something else, you need to
-   * wrap your store creating function into the corresponding middleware. For
-   * example, see the documentation for the `redux-thunk` package. Even the
-   * middleware will eventually dispatch plain object actions using this method.
+   * @param {Function} reducer A synchronous function that computes the next
+   * state based on the old state.
    *
-   * @param {Object} action A plain object representing “what changed”. It is
-   * a good idea to keep actions serializable so you can record and replay user
-   * sessions, or use the time travelling `redux-devtools`. An action must have
-   * a `type` property which may not be `undefined`. It is a good idea to use
-   * string constants for action types.
+   * @returns {Object} The final state after all the reducers have executed.
    *
-   * @returns {Object} For convenience, the same action object you dispatched.
-   *
-   * Note that, if you use a custom middleware, it may wrap `dispatch()` to
-   * return something else (for example, a Promise you can await).
    */
-  function dispatch(action) {
-    if (!isPlainObject(action)) {
+  function mutate(...mutations) {
+    mutations = Array.from(mutations)
+    if(mutations.some(mutation => typeof mutation !== 'function')) {
       throw new Error(
-        'Actions must be plain objects. ' +
-        'Use custom middleware for async actions.'
+        'Mutations must be one or more functions.'
       )
     }
 
-    if (typeof action.type === 'undefined') {
-      throw new Error(
-        'Actions may not have an undefined "type" property. ' +
-        'Have you misspelled a constant?'
-      )
-    }
-
-    if (isDispatching) {
-      throw new Error('Reducers may not dispatch actions.')
-    }
-
-    try {
-      isDispatching = true
-      currentState = currentReducer(currentState, action)
-    } finally {
-      isDispatching = false
-    }
+    currentState = mutations.reduce(
+      ( nextState, mutation ) => mutation(nextState),
+      getState()
+    )
 
     listeners.slice().forEach(listener => listener())
-    return action
+    return currentState
   }
-
-  /**
-   * Replaces the reducer currently used by the store to calculate the state.
-   *
-   * You might need this if your app implements code splitting and you want to
-   * load some of the reducers dynamically. You might also need this if you
-   * implement a hot reloading mechanism for Redux.
-   *
-   * @param {Function} nextReducer The reducer for the store to use instead.
-   * @returns {void}
-   */
-  function replaceReducer(nextReducer) {
-    currentReducer = nextReducer
-    dispatch({ type: ActionTypes.INIT })
-  }
-
-  // When a store is created, an "INIT" action is dispatched so that every
-  // reducer returns their initial state. This effectively populates
-  // the initial state tree.
-  dispatch({ type: ActionTypes.INIT })
 
   return {
-    dispatch,
+    mutate,
     subscribe,
-    getState,
-    replaceReducer
+    getState
   }
 }
